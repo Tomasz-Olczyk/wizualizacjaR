b878469bfa677ef292702972f1398738748aa62f
Tomasz Olczyk <t.olczyk@uw.edu.pl>
2025-03-23 13:35:58 +0100

    aktualizacja

---
+++ prezentacje i dashboardy/tinytext.R
@@ -0,0 +1,9 @@
+# Rmarkdown
+# tiny tet jest lekkim pakietem który umożliwia knit rmarkdown do pdf
+
+# oniższy pakiet  ma sprawić, że ma działać z pdf ale nie działa
+install.packages ("tinytex" )
+library(tinytex)
+
+# moze zadziała jak się użyje tego
+tinytex::install_tinytex()
